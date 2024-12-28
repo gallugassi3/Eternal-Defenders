a8aaae788bbae5cf4ba7d5a3c94e8cdfee15a5ab
Gal Lugassi <gallugassi3@gmail.com>
2024-12-28 18:23:42 +0200

    Added couple of buttons that help us build levels in the future and Grid Builder system that help us build the field in one button

--- Assets/Scripts/TileSystem/TileSlot.cs
+++ Assets/Scripts/TileSystem/TileSlot.cs
@@ -1,33 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TileSlot : MonoBehaviour
 {
     private MeshRenderer meshRenderer => GetComponent<MeshRenderer>();
     private MeshFilter meshFilter => GetComponent<MeshFilter>();
+    private Collider myCollider => GetComponent<Collider>();
+
+    private NavMeshSurface myNavMesh => GetComponentInParent<NavMeshSurface>();
 
     public void SwitchTile(GameObject referenceTile)
     {
+        gameObject.name = referenceTile.name;
+
         TileSlot newTile = referenceTile.GetComponent<TileSlot>();
 
         meshFilter.mesh = newTile.GetMesh();
         meshRenderer.material = newTile.GetMaterial();
 
-        foreach (GameObject obj in GetAllChildren())
-        {
-            DestroyImmediate(obj);
-        }
-
-        foreach (GameObject obj in newTile.GetAllChildren())
-        {
-            Instantiate(obj , transform);
-        }
+        UpdateCollider(newTile.GetCollider());
+        UpdateChildren(newTile);
+        UpdateLayer(referenceTile);
+        UpdateNavMesh();
     }
+
 
     public Material GetMaterial() => meshRenderer.sharedMaterial;
     public Mesh GetMesh() => meshFilter.sharedMesh;
-
+    public Collider GetCollider() => myCollider;
     public List<GameObject> GetAllChildren()
     {
         List<GameObject> children = new List<GameObject>();
@@ -37,6 +40,56 @@ public class TileSlot : MonoBehaviour
             children.Add(child.gameObject);
         }
         return children;
+    }
+
+    private void UpdateNavMesh() => myNavMesh.BuildNavMesh();
+    public void UpdateCollider(Collider newCollider)
+    {
+        DestroyImmediate(myCollider);
+
+        if (newCollider is BoxCollider)
+        {
+            BoxCollider original = newCollider.GetComponent<BoxCollider>();
+            BoxCollider myNewCollider = transform.AddComponent<BoxCollider>();
+
+            myNewCollider.center = original.center;
+            myNewCollider.size = original.size;
+        }
+        if (newCollider is MeshCollider)
+        {
+            MeshCollider original = newCollider.GetComponent<MeshCollider>();
+            MeshCollider myNewCollider = transform.AddComponent<MeshCollider>();
+
+            myNewCollider.sharedMesh = original.sharedMesh;
+            myNewCollider.convex = original.convex;
+        }
+    }
+    private void UpdateChildren(TileSlot newTile)
+    {
+        foreach (GameObject obj in GetAllChildren())
+        {
+            DestroyImmediate(obj);
+        }
+
+        foreach (GameObject obj in newTile.GetAllChildren())
+        {
+            Instantiate(obj, transform);
+        }
+    }
+
+    public void UpdateLayer(GameObject referenceObject) => gameObject.layer = referenceObject.layer;
+
+
+    public void RotateTile(int direction)
+    {
+        transform.Rotate(0, 90 * direction, 0);
+        UpdateNavMesh();
+    }
+
+    public void AdjustY(int verticalDirection)
+    {
+        transform.position += new Vector3(0, .1f * verticalDirection, 0);
+        UpdateNavMesh();
     }
 
 }
